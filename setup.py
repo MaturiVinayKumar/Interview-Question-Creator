bd5274420c52844fdb8abb810e011ccc073603f8
MaturiVinayKumar <vinaykumar3414.maturi@gmail.com>
2024-06-03 16:28:42 +0530

    Installed the required packages

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Sample layout Project for GENAI",
+    version= '0.0.0',
+    author="Maturi Vinay Kumar",
+    author_email="vinaykumar3414.maturi@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
